89d7cf9a562a0c009f9b5c7bf996339e3032e988
Kodobocz <kodoboczmatyas555@gmail.com>
2023-03-07 23:09:00 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("kurva sivatag/// geci");
+        System.out.println("kurva sivatag// geci");
     }
 }
